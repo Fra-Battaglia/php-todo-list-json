433f72cde749fc2633ad5e6afd21fe258d9ccc38
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-02 15:21:11 +0100

    style added, remove button added

--- index.php
+++ index.php
@@ -5,18 +5,36 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PHP ToDo List JSON</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 		<link rel="stylesheet" href="./css/style.css">
 		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	</head>
-	<body>
+	<body class="bg-dark text-light">
 
 		<div id="app">
+			<div class="container">
+				<div class="row vh-100 align-items-center justify-content-center">
+					<div class="col-5">
+						<h1 class="text-center">PHP ToDo List</h1>
+						<div class="list rounded">
+							<ul class="list-unstyled m-0 list-group">
 
-			<ul>
-				<li v-for="item in todo_list">{{ item.language }}</li>
-			</ul>
-			<input type="text" v-model="language" @keyup.enter="add_language">
-			<button type="submit" @click="add_language" >Invia</button>
+								<li v-for="item, index in todo_list" class="list-group-item p-2 d-flex justify-content-between align-items-center">
+									<div>{{ item.language }}</div>
+									<button class="btn btn-danger" @click="remove_language(index)"><i class="fa-solid fa-trash"></i></button>
+								</li>
+
+							</ul>
+						</div>
+						<div class="input-group mt-3">
+							<input type="text" class="form-control" placeholder="New Language" v-model="language" @keyup.enter="add_language">
+							<button class="btn btn-outline-warning" type="submit" id="button-addon2" @click="add_language">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
 		</div>
 
 		<script src="https://unpkg.com/vue@3"></script>
--- server.php
+++ server.php
@@ -6,14 +6,32 @@
 	// Converto il json in codice php
 	$todo_list = json_decode($string, true);
 
-	if(isset($_POST['todo_input'])) {
+	// Aggiungi voci alla lista
+	if(isset($_POST['todo_input']) && $_POST['todo_input'] != '') {
 		$todo_input = $_POST['todo_input'];
 		$todo_list[] = ["language" => $todo_input, "done" => false];
 
-		file_put_contents('todo-list.json', json_encode($todo_list));
+		file_put_contents('todo-list.json', json_encode($todo_list, JSON_PRETTY_PRINT));
 	}
+
+	if(isset($_POST['delete'])) {
+		unset($todo_list[$_POST['delete']]);
+
+		file_put_contents('todo-list.json', json_encode($todo_list, JSON_PRETTY_PRINT));
+	}
+
+	// if(isset($_POST['edit'])) {
+	// 	// Individua l'elemento avente indice contenuto in $_POST['edit'] e lo sostituisce con il suo valore corrispondente che in questo caso è l'array
+	// 	$replacement = array(
+	// 		$_POST['edit'] => array(
+	// 			"language" => $_POST['language_edit'], 
+	// 			"done" => false
+	// 		)
+	// 	);
+	// 	$todo_list = array_replace($todo_list, $replacement);
+	// }
 
 	header('Content-Type: application/json');
 	echo json_encode($todo_list);
-	
+
 ?>
